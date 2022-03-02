dcaab03cc6f68cfc13f3ef222f0aa7cde2ca12a5
Brian Naperkoski <cusstom180@yahoo.com>
2022-03-01 20:02:23 -0500

    starting to make markers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API;
@@ -39,6 +40,7 @@ export default function App() {
       setZoom(map.current.getZoom().toFixed(2));
     });
   });
+
 
   return (
     <div>
