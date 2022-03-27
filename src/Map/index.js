e0e6975495ab1990476965a887fd375943a9d07e
Brian Naperkoski <cusstom180@yahoo.com>
2022-03-27 14:08:23 -0400

    added layers and sources in mapbox

--- src/Map/index.js
+++ src/Map/index.js
@@ -2,66 +2,29 @@ import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl, { Source, Layer } from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
-import Route from '../Data/Routes/Stoney_Dude_Fuel_EX_9_8_.json'
 
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API;
 
-export default function App() {
+const App = () => {
     const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
+    const [lng, setLng] = useState(-83.1129);
+    const [lat, setLat] = useState(42.6901);
     const [zoom, setZoom] = useState(9);
 
-    const geojson = {
-        'type': 'FeatureCollection',
-        'data':
-        {
-            'geometry': {
-                'type': 'LineString',
-                'coordinates': [
-                    [-122.483696, 37.833818],
-                    [-122.483482, 37.833174],
-                    [-122.483396, 37.8327],
-                    [-122.483568, 37.832056],
-                    [-122.48404, 37.831141],
-                    [-122.48404, 37.830497],
-                    [-122.483482, 37.82992],
-                    [-122.483568, 37.829548],
-                    [-122.48507, 37.829446],
-                    [-122.4861, 37.828802],
-                    [-122.486958, 37.82931],
-                    [-122.487001, 37.830802],
-                    [-122.487516, 37.831683],
-                    [-122.488031, 37.832158],
-                    [-122.488889, 37.832971],
-                    [-122.489876, 37.832632],
-                    [-122.490434, 37.832937],
-                    [-122.49125, 37.832429],
-                    [-122.491636, 37.832564],
-                    [-122.492237, 37.833378],
-                    [-122.493782, 37.833683]
-                ]
-            }
-        }
-    };
-
-
     useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
+        const map = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [lng, lat],
             zoom: zoom
         });
-        const mapScope = map.current;
+
+        if (!map) return; // wait for map to initialize
 
         const geocoder = new MapboxGeocoder({
-            // flyTo: false,
             accessToken: mapboxgl.accessToken,
             mapboxgl: mapboxgl,
             marker: {
@@ -69,73 +32,49 @@ export default function App() {
             }
         });
 
-        mapScope.addControl(geocoder);
+        map.addControl(geocoder);
 
+        // still not working
+        // get center of map and ask google for info
         geocoder.on('results', function (results) {
             //console.log(results);
-        });
-
-        map.current.on('moveend', () => {
-            console.log(`lat: ${lat}, lng: ${lng}`);
-
-            map.current.addSource('stoney-route', {
-                type: 'geojson',
-                data: {
-                    'type': 'Feature',
-                    'geometry': {
-                        'type': 'Polygon',
-                        // These coordinates outline Maine.
-                        'coordinates': [
-                            [
-                                [-67.13734, 45.13745],
-                                [-66.96466, 44.8097],
-                                [-68.03252, 44.3252],
-                                [-69.06, 43.98],
-                                [-70.11617, 43.68405],
-                                [-70.64573, 43.09008],
-                                [-70.75102, 43.08003],
-                                [-70.79761, 43.21973],
-                                [-70.98176, 43.36789],
-                                [-70.94416, 43.46633],
-                                [-71.08482, 45.30524],
-                                [-70.66002, 45.46022],
-                                [-70.30495, 45.91479],
-                                [-70.00014, 46.69317],
-                                [-69.23708, 47.44777],
-                                [-68.90478, 47.18479],
-                                [-68.2343, 47.35462],
-                                [-67.79035, 47.06624],
-                                [-67.79141, 45.70258],
-                                [-67.13734, 45.13745]
-                            ]
-                        ]
-                    }
-                }
-            });
-
-            map.current.addLayer({
-                'id': 'route',
-                'type': 'line',
-                'source': 'stoney-route',
-                'line-color': 'orange'
+            map.on('moveend', () => {
+                console.log(`result: ${results}`);
+                console.log(`lat: ${lat}, lng: ${lng}`);
             });
 
         });
 
+        map.on('load', () => {
+            map.addSource('basic-copy', {
+                type: 'vector',
+                url: 'mapbox://cusston180.cl19gpgg52g8k28mknu2q009p-2gc4v'
+            })
 
-
-    });
-
-    useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-
+            map.addLayer({
+                id: 'basic-copy-layer',
+                type: 'line',
+                source: 'basic-copy',
+                paint: {
+                    "line-color": "#15cc09",
+                    "line-width": 4
+                }, 'source-layer': 'dirt-roads-by-tom'
+            });
         });
 
-    });
+
+        map.on('move', () => {
+            setLng(map.getCenter().lng.toFixed(4));
+            setLat(map.getCenter().lat.toFixed(4));
+            setZoom(map.getZoom().toFixed(2));
+        });
+
+
+
+
+        // cleanup function to remove map on unmount
+        return () => map.remove();
+    }, []);
 
     return (
         <div>
@@ -145,4 +84,73 @@ export default function App() {
             <div ref={mapContainer} className="map-container" />
         </div>
     );
-}
+};
+
+export default App;
+
+// export default function App() {
+
+//     useEffect(() => {
+//         if (map.current) return; // initialize map only once
+//         map.current = new mapboxgl.Map({
+//             container: mapContainer.current,
+//             style: 'mapbox://styles/mapbox/streets-v11',
+//             center: [lng, lat],
+//             zoom: zoom
+//         });
+
+
+
+//         const geocoder = new MapboxGeocoder({
+//             // flyTo: false,
+//             accessToken: mapboxgl.accessToken,
+//             mapboxgl: mapboxgl,
+//             marker: {
+//                 color: 'orange'
+//             }
+//         });
+
+//         mapScope.addControl(geocoder);
+
+//         geocoder.on('results', function (results) {
+//             //console.log(results);
+//             map.current.on('moveend', () => {
+//                 console.log(`lat: ${lat}, lng: ${lng}`);
+//             });
+
+//         });
+
+
+
+//         // map.current.addSource('Basic-copy', {
+//         //     type: 'vector',
+//         //     url: 'mapbox://cusston180.cl19gpgg52g8k28mknu2q009p-2gc4v'
+//         // })
+
+//         if (!map.current) return; // wait for map to initialize
+//         map.current.on('move', () => {
+//             setLng(map.current.getCenter().lng.toFixed(4));
+//             setLat(map.current.getCenter().lat.toFixed(4));
+//             setZoom(map.current.getZoom().toFixed(2));
+
+//         });
+//     });
+
+
+//     return (
+//         <div>
+//             <div className="sidebar">
+//                 Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+//             </div>
+//             <div ref={mapContainer} className="map-container" />
+//         </div>
+//     );
+// }
+
+        // if (!map.current) return; // wait for map to initialize
+        // map.current.on('move', () => {
+        //     setLng(map.current.getCenter().lng.toFixed(4));
+        //     setLat(map.current.getCenter().lat.toFixed(4));
+        //     setZoom(map.current.getZoom().toFixed(2));
+
+        // });
