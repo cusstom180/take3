8c8a78d9ef19bfa7986b0de1168cd12fe7b56e67
Brian Naperkoski <cusstom180@yahoo.com>
2022-03-29 21:07:44 -0400

    working on adding markers form datasets

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl, { Source, Layer } from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl, { Marker, Source, Layer } from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 
